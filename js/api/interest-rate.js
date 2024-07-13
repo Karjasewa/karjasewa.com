2fcc7943918a5d6499e18fc5a3fac4eee767e471
Rajesh Thapa <devrajeshthapa@gmail.com>
2024-07-13 16:39:21 +0545

    Done some minor changes

---
+++ js/api/interest-rate.js
@@ -0,0 +1,17 @@
+let tbody = document.querySelector("tbody");
+
+fetch("https://karjasewa.com/js/json/interest-rate.json").then(response => {
+    return response.json();
+}).then(data => {
+    data.forEach(data => {
+        let { sn, bankName,} = data;
+
+        let html = `<tr>
+                    <td data-label="S.N">${sn}</td>
+                    <td data-label="Bank Name">${bankName}</td>
+                    <td class="empty"></td>
+                </tr>`;
+        tbody.innerHTML += html;
+    });
+})
+
