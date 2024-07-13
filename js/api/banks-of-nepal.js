a11125673e19a5eca8c1b163c997896907282172
Rajesh Thapa <devrajeshthapa@gmail.com>
2024-07-13 16:57:53 +0545

    Done some minor changes

--- js/api/banks-of-nepal.js
+++ js/api/banks-of-nepal.js
@@ -4,7 +4,7 @@ fetch("https://karjasewa.com/js/json/banks-of-nepal.json").then(response => {
     return response.json();
 }).then(data => {
     data.forEach(data => {
-        let { sn, bankName} = data;
+        let { sn, bankName, } = data;
 
         let html = `<tr>
                     <td data-label="S.N">${sn}</td>
--- js/api/interest-rate.js
+++ js/api/interest-rate.js
@@ -4,11 +4,13 @@ fetch("https://karjasewa.com/js/json/interest-rate.json").then(response => {
     return response.json();
 }).then(data => {
     data.forEach(data => {
-        let { sn, bankName,} = data;
+        let { sn, bankName, baseRate, effectiveRate} = data;
 
         let html = `<tr>
                     <td data-label="S.N">${sn}</td>
                     <td data-label="Bank Name">${bankName}</td>
+                    <td data-label="Bank Name">${baseRate}</td>
+                    <td data-label="Bank Name">${effectiveRate}</td>
                     <td class="empty"></td>
                 </tr>`;
         tbody.innerHTML += html;
